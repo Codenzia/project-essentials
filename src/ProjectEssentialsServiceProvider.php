072dae6c6d1facd036780bc3e05eaa72591e7c95
sehsah <8730764+sehsah@users.noreply.github.com>
2025-10-28 14:54:11 +0000

    Fix styling

--- src/ProjectEssentialsServiceProvider.php
+++ src/ProjectEssentialsServiceProvider.php
@@ -12,11 +12,11 @@ use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Blade;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Illuminate\Support\Facades\Blade;
 
 class ProjectEssentialsServiceProvider extends PackageServiceProvider
 {
